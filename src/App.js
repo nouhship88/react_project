7055c04b7e455891d19384f475d4297608bcd98f
Noah Shanta <nouhship88@gmail.com>
2024-12-15 22:44:36 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Welcome from './Welcome';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to My First React App!</h1>
+      <p>This is my first React project.</p>
+      <Welcome />
+
+    </div>
+  );
+
+  
+  
+}
+
+export default App;
